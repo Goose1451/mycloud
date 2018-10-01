d9034303e53f247556fae4a91563b5e73e1701fd
Goose1451 <wgodard@gmail.com>
2018-10-01 14:20:07 -0700

    changes

--- buttontest.py
+++ buttontest.py
@@ -1,5 +1,6 @@
 import RPi.GPIO as GPIO
 from neopixel import *
+import sys, time
 
 LED_COUNT    =20
 LED_PIN      =18
@@ -19,9 +20,40 @@ def colorAll(strip, color):
         strip.setPixelColor(i, color)
     strip.show()
 
-while True:
-    if GPIO.input(10) == GPIO.HIGH:
-        #print("Button was pushed!")
-        colorAll(strip, Color(0, 90, 0))
+def wheel(pos):
+    if pos < 85:
+        return Color(pos * 3, 255 - pos * 3, 0)
+    elif pos < 170:
+        pos -= 85
+        return Color(255 - pos * 3, 0, pos * 3)
     else:
-        colorAll(strip, Color(0, 0, 0))
+        pos -= 170
+        return Color(0, pos * 3, 255 - pos * 3)
+
+def theaterChaseRainbow(strip, wait_ms=50):
+    for j in range(256):
+        for q in range(3):
+            for i in range(0, strip.numPixels(), 3):
+                strip.setPixelColor(i+q, wheel((i+j) % 255))
+            strip.show()
+            time.sleep(wait_ms/1000.0)
+            for i in range(0, strip.numPixels(), 3):
+                strip.setPixelColor(i+q, 0)
+
+def rainbowCycle(strip, wait_ms=20, iterations=1):
+    for j in range(256 * iterations):
+        for i in range(strip.numPixels()):
+            strip.setPixelColor(i, wheel((int(i * 256 / strip.numPixels()) + j) & 255))
+        strip.show()
+        time.sleep(wait_ms/1000.0)
+
+try:
+    while True:
+        if GPIO.input(10) == GPIO.HIGH:
+            #print("Button was pushed!")
+            #colorAll(strip, Color(0, 90, 0))
+            rainbowCycle(strip)
+        else:
+            colorAll(strip, Color(0, 0, 0))
+except KeyboardInterrupt:
+    colorAll(strip, Color(0, 0, 0))
